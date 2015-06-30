8930e21291f567a3f132089569fe720e894a8a53
Simon Pickartz <spickartz@eonerc.rwth-aachen.de>
2015-06-30 15:16:06 +0200

    cosmetic changes in system output

--- info.c
+++ info.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdint.h>
+#include <ctype.h>
 #include <sys/sysinfo.h>
 
 #include "info.h"
@@ -60,7 +61,7 @@ int info_print()
 
     /* Hostname */
     gethostname(str, STR_SIZE);
-    OM_COMMENT; fprintf(options.output_file_stream, "local hostname:   %s\n", str);
+    OM_COMMENT; fprintf(options.output_file_stream, "local hostname     : %s\n", str);
 
     /* CPU info */
     asm volatile ( "mov $0x80000000, %%eax\ncpuid" : "=a"(a), "=b"(b), "=c"(c), "=d"(d) );
@@ -76,30 +77,32 @@ int info_print()
         asm volatile ( "mov $0x80000004, %%eax\ncpuid" : "=a"(((unsigned int *)&brand_string)[8]), "=b"(((unsigned int *)&brand_string)[9]), "=c"(((unsigned int *)&brand_string)[10]), "=d"(((unsigned int *)&brand_string)[11]) );
         brand_string[4*4*3] = '\0';
     }
-    OM_COMMENT; fprintf(options.output_file_stream, "CPU brand string: %s\n", brand_string);
+    char *brand_string_ptr = brand_string;
+    while(isspace(*brand_string_ptr)) brand_string_ptr++;
+    OM_COMMENT; fprintf(options.output_file_stream, "CPU brand string   : %s\n", brand_string_ptr);
 
     /* total memory */
     // ...
 
-    OM_COMMENT; fprintf(options.output_file_stream, "Invariant TSC:     %d\n", information.invariant_tsc);
-    OM_COMMENT; fprintf(options.output_file_stream, "TSC per usec:      %ld\n", information.tsc_per_usec);
-    OM_COMMENT; fprintf(options.output_file_stream, "TSC read overhead: %ld\n", information.tsc_diff);
+    OM_COMMENT; fprintf(options.output_file_stream, "Invariant TSC      : %d\n", information.invariant_tsc);
+    OM_COMMENT; fprintf(options.output_file_stream, "TSC per usec       : %ld\n", information.tsc_per_usec);
+    OM_COMMENT; fprintf(options.output_file_stream, "TSC read overhead  : %ld\n", information.tsc_diff);
 
     if (information.nbr_cpus > -1) {
-        OM_COMMENT; fprintf(options.output_file_stream, "Nbr. of CPUs:     %d\n", information.nbr_cpus);
-        OM_COMMENT; fprintf(options.output_file_stream, "Nbr. CPUs conf.:  %d\n", get_nprocs_conf());        /* sys/sysinfo.h */
+        OM_COMMENT; fprintf(options.output_file_stream, "Nbr. of CPUs       : %d\n", information.nbr_cpus);
+        OM_COMMENT; fprintf(options.output_file_stream, "Nbr. CPUs conf.    : %d\n", get_nprocs_conf());        /* sys/sysinfo.h */
 
         //OM_COMMENT; fprintf(options.output_file_stream, "Nbr. phys. pages: %ld\n", get_phys_pages());
         //OM_COMMENT; fprintf(options.output_file_stream, "Nbr. avail. p. p.:%ld\n", get_avphys_pages());
         
-        OM_COMMENT; fprintf(options.output_file_stream, "Mem in System:    %ld MiB\n", get_phys_pages() * 4 / 1024);     /* sys/sysinfo.h */
-        OM_COMMENT; fprintf(options.output_file_stream, "Mem available:    %ld MiB\n", get_avphys_pages() * 4 / 1024);   /* sys/sysinfo.h */
+        OM_COMMENT; fprintf(options.output_file_stream, "Mem in System      : %ld MiB\n", get_phys_pages() * 4 / 1024);     /* sys/sysinfo.h */
+        OM_COMMENT; fprintf(options.output_file_stream, "Mem available      : %ld MiB\n", get_avphys_pages() * 4 / 1024);   /* sys/sysinfo.h */
         
     }
     
     /* Optimizing */
 #if defined (CC) && defined(OPT)
-    OM_COMMENT; fprintf(options.output_file_stream, "Compiler:         %s -O%s\n", CC, OPT);
+    OM_COMMENT; fprintf(options.output_file_stream, "Compiler           : %s -O%s\n", CC, OPT);
 #endif
     
     return 0;
